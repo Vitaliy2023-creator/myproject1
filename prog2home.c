36f7ea63d729c2ea8cd77cc5a9fb45e7509f5d17
Vitaliy2023-creator <144263129+Vitaliy2023-creator@users.noreply.github.com>
2023-10-02 11:17:49 +0300

    Update prog2home.c

--- prog2home.c
+++ prog2home.c
@@ -1,19 +1,15 @@
 #include <stdio.h>
+#include<locale.h>
 int main()
 {
+    setlocale(LC_CTYPE, "RUS");
     float L = 393;
     float n = 4;
     printf("Дано:\n");
-    printf("%10d\n",(int)n);
-    printf("%10d\n",(int)L);
-    printf("%15s\n","_________");
+    printf("  %10d\n", (int)n);
+    printf("  %10d\n", (int)L);
+    printf(" \t_________\n");
     printf("Ответ:\n");
-    printf("%6s","");
-    // вычисляем целую часть дроби
-    printf("+");
-    printf("%02d.",(int)n/(int)L);
-    // вычисляем дробную часть
-    
-    printf("%06d", (int)(((n/L)-(int)n/(int)L) *1000000));
+    printf("\t%+010.6f", n / L);
     return 0;
 }
